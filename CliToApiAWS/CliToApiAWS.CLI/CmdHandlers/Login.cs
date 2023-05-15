8c5593fdc2d578b7d5428eb8fc983a405249e410
drlsn <dariusz.lusnia@gmail.com>
2023-05-15 23:47:55 +0200

    Add first

---
+++ CliToApiAWS/CliToApiAWS.CLI/CmdHandlers/Login.cs
@@ -0,0 +1,30 @@
+﻿using Corelibs.CLI;
+using Corelibs.Cognito;
+
+namespace ManabuX.Client.CLI.Views;
+
+internal class Login : ICommandHandler
+{
+    private readonly IAuthentication _auth;
+
+    public Login(
+        IAuthentication authUser)
+    {
+        _auth = authUser;
+    }
+
+    public async Task<string> Process(string[] args)
+    {
+        Console.Write("Username: ");
+        var username = Console.ReadLine();
+
+        Console.Write("Password: ");
+        var password = Console.ReadLine();
+
+        await _auth.SignIn(username, password);
+
+        return $"Login successful";
+    }
+
+    public string Command => "login";
+}
